293d53334f52ba967ec7f442675a52dc43f270f1
Rodrigo Bossini <professorbossini@gmail.com>
2022-03-24 18:02:18 -0300

    Terminamos a apostila 05, de OO basica com o exemplo de LivroDeNotas

--- LivroDeNotas.java
+++ LivroDeNotas.java
@@ -1,16 +1,16 @@
 public class LivroDeNotas{
     
-    String nomeDoCurso;
+    private String nomeDoCurso;
 
-    // public LivroDeNotas(){
-
-    // }
-
-    public LivroDeNotas(String nomeDoCurso){
-        nomeDoCurso = nomeDoCurso;
+    public String getNomeDoCurso(){
+        return nomeDoCurso;
     }
-    
+
+    public void setNomeDoCurso(String nomeDoCurso){
+        this.nomeDoCurso = nomeDoCurso;
+    }
+
     public void exibirMensagem(){
-        System.out.printf("Bem vindo ao livro de notas de %s\n", nomeDoCurso);
+        System.out.printf("Bem vindo ao livro de notas de %s\n", this.nomeDoCurso);
     } 
 }
--- Principal.java
+++ Principal.java
@@ -1,28 +1,7 @@
-// int soma (int a, int b){
-//     return a + b;
-// }
-
-// int res = soma(1, "2");
-
-import static javax.swing.JOptionPane.showInputDialog;
 public class Principal {
     public static void main(String[] args) {
-
-        String nomeDoCurso = showInputDialog("Digite o nome do curso");
-        LivroDeNotas livro = new LivroDeNotas(nomeDoCurso);
-        livro.nomeDoCurso = nomeDoCurso;
-        livro.exibirMensagem();
-
-
-
-    //     String nomeDoCurso = showInputDialog("Digite o nome do curso");
-    //    LivroDeNotas livro = new LivroDeNotas();
-    //    livro.nomeDoCurso = nomeDoCurso;
-    //    livro.exibirMensagem();
-
-    //    LivroDeNotas livro2 = new LivroDeNotas();
-    //    livro2.nomeDoCurso = showInputDialog("Digite o segundo nome");
-    //    livro2.exibirMensagem();
-
+        LivroDeNotas livro = new LivroDeNotas();
+        livro.setNomeDoCurso("POO");
+        System.out.println(livro.getNomeDoCurso());
     }
 }
